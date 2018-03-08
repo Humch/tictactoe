5fb173c3bc3cc3907ad184f9480ad4015e715041
root <root@DEBXXX>
2018-03-08 10:30:44 +0100

    update

--- tictactoe.js
+++ tictactoe.js
@@ -19,12 +19,10 @@ $(".symbol").click(function() {
         $('#start-modal').modal('hide');
         var startButton = document.getElementById("start-game");
         startButton.disabled = true;
-    
         playGame();
     });
 
 $(".game-case").click(function() {
-    console.log("this is id :" + this.id);
     var gameCase = document.getElementById(this.id);
     if (playerTurn && choosenSymbol !== "" && boardGame[this.id] === 0 && endGame === false) {
         var gameCaseSymbol = document.createElement('i');
@@ -32,12 +30,9 @@ $(".game-case").click(function() {
         
         var gameCaseP = document.createElement('p');
         gameCaseP.className = "text-center display-1";
-        
         gameCaseP.appendChild(gameCaseSymbol);
         gameCase.appendChild(gameCaseP);
-        
         boardGame[this.id] = 1;
-        
         playerTurn = false;
         player = 0;
         testVictory();
@@ -61,12 +56,9 @@ function playGame() {
 
 function ComputerPlay() {
         if (!endGame) {
-                console.log("moot !");
                 $("#computer-turn").show(800);
                 $("#player-turn").hide(800);
                 setTimeout(function() {
-                        
-                        console.log("wait!!!");
                         var chooseCase = false;
                         while(!chooseCase) {
                                 var rand = Math.floor(Math.random() * boardGame.length);
@@ -91,8 +83,10 @@ function ComputerPlay() {
                                 }
                         }
                         testVictory();
+                        if (!endGame) {
                         $("#player-turn").show(800);
                         $("#computer-turn").hide(800);
+                        }
                 },3000);
         }
 }
@@ -100,31 +94,27 @@ function ComputerPlay() {
 function arraysEqual(a, b,c) {
         var tester = a.slice();
         for (var j = 0; j < tester.length; ++j) {
-                if ((c === 1) && (tester[j] === 1)) { tester[j] = 0;} else if ((c === 2) && (tester[j] === 2)) { tester[j] = 0;}
+                if ((c === 1 && tester[j] === 1) || b[j] === 0) { tester[j] = 0;} else if ((c === 2 && tester[j] === 2) || b[j] === 0) { tester[j] = 0;}
         }
-        
         for (var i = 0; i < tester.length; ++i) {
                 if ((c === 1) && (tester[i] === 1)) { tester[i] = 0;} else if ((c === 2) && (tester[i] === 2)) { tester[i] = 0;} 
-                
-                console.log("test " + i + " : " + a + " : " + tester + " : " + b);
                 if ((tester[i] !== b[i])) return false;
         }
         return true;
 }
 
 function testVictory() {
-        console.log("test victory !");
         for (var i = 0; i < winBoard.length; ++i) {
                 var resultA = arraysEqual(boardGame,winBoard[i],2);
-                console.log("resultA : " + resultA);
                 if (resultA) {
+                        endGame = true;
                         endTheGame("victory");
                 }
         }
         for (var j = 0; j < loseBoard.length; ++j) {
                 var resultB = arraysEqual(boardGame,loseBoard[j],1);
-                console.log("resultB : " + resultB);
                 if (resultB) {
+                        endGame = true;
                         endTheGame("lose");
                 }
         }
@@ -134,27 +124,26 @@ function testVictory() {
                         equal++;
                 }
         }
-        if (equal === boardGame.length) {
+        if (equal === boardGame.length && !endGame) {
                 endTheGame("equal");
         }
 }
 
 function endTheGame(result) {
-        console.log(result);
         $("#player-turn").hide(800);
         $("#computer-turn").hide(800);
-        endGame = true;
         if (result == "victory") {
-            var alertStyle = "alert-success"
+            var alertStyle = "alert-success";
             var alertText = "You win";
         } else if (result == "lose") {
-            var alertStyle = "alert-danger"
+            var alertStyle = "alert-danger";
             var alertText = "You lose !!";
         } else if (result == "equal") {
-            var alertStyle = "alert-warning"
+            var alertStyle = "alert-warning";
             var alertText = "Draw game !!";
         }
         var z = document.getElementById("end-game");
+        if (z.classList[alertStyle]) {z.classList.remove(alertStyle);}
         z.classList.add(alertStyle);
         z.innerText = alertText;
         $("#end-game").show(800);
